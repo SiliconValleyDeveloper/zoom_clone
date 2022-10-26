1893f24a59484fb2d618193ae9ceb0f7dba46e42
Ankit <siliconvalleydeveloper>
2022-10-26 15:02:41 +0530

    zoom integration

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:zoom_clone/resources/auth_methods.dart';
+import 'package:zoom_clone/screen/history_meeting_screen.dart';
+import 'package:zoom_clone/screen/meeting_screen.dart';
+import 'package:zoom_clone/widgets/custom_button.dart';
+
+import '../utils/colors.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _page = 0;
+  onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  List<Widget> pages = [
+    MeetingScreen(),
+    HistoryMeetingScreen(),
+    const Text('Contacts'),
+    CustomButton(text: 'Log Out', onPressed: () => AuthMethods().signOut()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        title: const Text('Meet & Chat'),
+        centerTitle: true,
+      ),
+      body: pages[_page],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: footerColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        onTap: onPageChanged,
+        currentIndex: _page,
+        type: BottomNavigationBarType.fixed,
+        unselectedFontSize: 14,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.comment_bank,
+            ),
+            label: 'Meet & Char',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.lock_clock,
+            ),
+            label: 'Meetings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+            ),
+            label: 'Contacts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings_outlined,
+            ),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
